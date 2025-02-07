d18fcfcba8820e473297ebf9b1e7b7a570f51c7e
JISUNG PARK <meowbutlerdev@gmail.com>
2025-02-07 15:15:08 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 박지성 on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
